85a0e0e2a81f8984261d43cb255af8da72e920c2
Hisao Setoguchi <h.setoguchi@arent3d.com>
2025-05-09 01:22:27 +0000

    add a comment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
-require "minitest/reporters"
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
@@ -10,6 +10,8 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  # ApplicationHelperを追加
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
