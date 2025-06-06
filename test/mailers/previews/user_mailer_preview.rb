1cb7f2f623f6054bbadb1e87ae3cb572f85af43e
Hisao Setoguchi <a25125hs@aiit.ac.jp>
2025-06-06 12:58:03 +0000

    Implement password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -9,6 +9,8 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at https://glorious-doodle-qvq7vvwx6vh9x9x-3000.app.github.dev/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
